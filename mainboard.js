c597f18888ffed3946dc4973ea9773164615f11e
anh.tran(c0720i1) <69283798+theanh111@users.noreply.github.com>
2020-10-26 14:18:43 +0700

    Bai tap CSS

---
+++ mainboard.js
@@ -0,0 +1,15 @@
+function story1() {
+    location.href = "html/BanOQue.html";
+}
+
+function story2() {
+    location.href = "html/HaOi.html";
+}
+
+function story3() {
+    location.href = "html/MotThoiXuaCu.html";
+}
+
+function story4() {
+    location.href = "html/ChenhVenh.html";
+}
